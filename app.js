e43fb552163f8d0c284c9c832f6313c5aaa3d341
Bilal Akbar <geo.bilal7@gmail.com>
2023-01-16 22:20:22 +0500

    Complete project

---
+++ app.js
@@ -0,0 +1,63 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const _= require("lodash");
+// eslint-disable-next-line no-undef
+const handlingItems = require(__dirname + "/external_modules/handlingItems.js");
+const app = express();
+app.set("view engine", "ejs"); // to tell we are using ejs for dynamic binding
+app.use(bodyParser.urlencoded({ extended: true })); //to parse the body of the html and store it into req.body
+app.use(express.static("public")); //When a client makes a request for a file in this directory, the middleware will check if the file exists and, if it does, send it to the client with the appropriate Content-Type header.
+mongoose.set("strictQuery", true); //to avoid the deprecation warning
+mongoose.connect(
+	"mongodb+srv://admin-bilal:test123@atlascluster.kvhg4ya.mongodb.net/todolistDB"
+);
+const itemSchema = new mongoose.Schema({
+	name: {
+		type: String
+	},
+});
+const Item = mongoose.model("item", itemSchema);
+const customListSchema = new mongoose.Schema({
+	name: String,
+	items: [itemSchema]
+});
+const customList = mongoose.model("customList", customListSchema);
+const port = 3000;
+app.get("/", function (req, res) {
+	handlingItems.listTitle = handlingItems.day;
+	Item.find({}, function (err, items) {
+		if (err) console.log(err);
+		else res.render("list", { listTitle: handlingItems.day, newItems: items });
+	});
+});
+function createandDisColl(theList, res) {
+	customList.findOne({ name: theList }, async function (err, result) {
+		if (!result) {
+			const newDocument = new customList({
+				name: theList,
+			});
+			await newDocument.save();
+			res.render("list", { listTitle: handlingItems.listTitle, newItems: newDocument.items});
+		}
+		else {
+			res.render("list", { listTitle: handlingItems.listTitle, newItems: result.items });
+		}
+	});
+}
+app.get("/:list", function (req, res) {
+	const theList = _.capitalize(req.params.list);
+	handlingItems.listTitle = `${theList}`;
+	createandDisColl(theList, res);
+});
+app.post("/", function (req, res) {
+	const item = req.body.newItem;
+	// console.log(handlingItems.listTitle);
+	if (item !== "" && handlingItems.listTitle !== handlingItems.day) {
+		handlingItems.handlingPostRequest(customList, handlingItems.listTitle, item, req, res);
+	} else {
+		handlingItems.handlingPostRequest(Item, "items", item, req, res);
+	}
+});
+// handling the post request
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ external_modules/handlingItems.js
@@ -0,0 +1,56 @@
+// variables
+let listTitle;
+//functions
+async function handlingPostRequest(dataBase, collName, item, req, res) {
+	let items;
+	if (dataBase.collection.name === "items" && item === undefined) {
+		await dataBase.deleteOne({ _id: req.body.deletionButton }).exec((err) => {
+			if (err) console.log("Error in deleting the item");
+			else console.log("successfully deleted the item");
+		});
+		res.redirect("/");
+	} else if (item === undefined) {
+		const collection = await dataBase
+			.findOneAndUpdate(
+				{ name: collName },
+				{
+					$pull: { items: { _id: req.body.deletionButton } },
+				},
+				{ new: true }
+			)
+			.exec((err) => {
+				if (err) console.log("Error in deleting the item");
+				else console.log("successfully deleted the item");
+			});
+		res.redirect(`/${collName}`);
+	}
+	//Adding new items to the list
+	else if (dataBase.collection.name === "items" && item !== undefined) {
+		const newItem = new dataBase({
+			name: item,
+		});
+		await newItem.save();
+		res.redirect("/");
+	} else {
+		const foundedDB = await dataBase.findOneAndUpdate(
+			{ name: collName },
+			{ $push: { items: { name: item } } },
+			{ new: true }
+		);
+		res.redirect(`/${foundedDB.name}`);
+	}
+}
+const today = new Date();
+const option = {
+	weekday: "short",
+	day: "numeric",
+	month: "long",
+	year: "2-digit",
+};
+const day = today.toLocaleDateString("en-US", option); //converts the date w.r.t to the values given in the option object
+//the exporting object
+module.exports = {
+	listTitle: listTitle,
+	handlingPostRequest: handlingPostRequest,
+	day: day,
+};
